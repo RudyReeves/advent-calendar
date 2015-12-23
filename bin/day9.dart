db9a9656c1fbcf90750ba162da626174f6347053
Rudy Reeves <rudyreeves@lclark.edu>
2015-12-23 03:32:42 -0500

    Refactored code

--- bin/day9.dart
+++ bin/day9.dart
@@ -2,73 +2,53 @@ import 'dart:io' show File;
 import 'dart:collection' show LinkedHashMap;
 
 List<String> lines;
-List<City> cities;
-List<City> visited;
+List<String> visited;
+Map<String, Map<String, int>> paths = {};
 
 main() async {
   lines = await new File('inputs/day9_input.txt').readAsLines();
   print("Part 1: ${await bestTravelDistance()}");
   print("Part 2: ${await bestTravelDistance(closest: false)}");
+  await parseFile();
 }
 
-readFile() async {
-  cities = [];
+parseFile() async {
   visited = [];
   for (String line in lines) {
     List<String> tokens = line.split(' ');
-    var place1 = City.getByName(tokens[0]);
-    var place2 = City.getByName(tokens[2]);
     int distance = int.parse(tokens[4]);
-
-    place1.distances[place2] = distance;
-    place2.distances[place1] = distance;
+    if (paths[tokens[0]] == null) paths[tokens[0]] = {};
+    if (paths[tokens[2]] == null) paths[tokens[2]] = {};
+    paths[tokens[0]][tokens[2]] = distance;
+    paths[tokens[2]][tokens[0]] = distance;
   }
 }
 
-class City {
-  final String name;
-  Map<City, int> distances;
-  City(this.name, this.distances);
 
-  static getByName(String name) {
-    var lookup = cities.where((place) => place.name == name);
-    if (lookup.isEmpty) {
-      City place = new City(name, {});
-      cities.add(place);
-      return place;
-    } else {
-      return lookup.single;
-    }
-  }
-
-  getNext({bool closest: true}) {
-    sorter(p1, p2) => (distances[p1] - distances[p2]) * (closest ? 1 : -1);
-    var keys = distances.keys.toList()..sort(sorter);
-    var sorted = new LinkedHashMap.fromIterable(keys, value: (v) => distances[v]);
-    return sorted.keys.firstWhere((p) => !visited.contains(p), orElse: () => null);
-  }
+getNext(String city, {bool closest: true}) {
+  sorter(city1, city2) => (paths[city][city1] - paths[city][city2]) * (closest ? 1 : -1);
+  var keys = paths[city].keys.toList()..sort(sorter);
+  var sorted = new LinkedHashMap.fromIterable(keys, value: (v) => paths[city][v]);
+  return sorted.keys.firstWhere((p) => !visited.contains(p), orElse: () => null);
 }
 
 bestTravelDistance({bool closest: true}) async {
-  await readFile();
+  await parseFile();
   var distances = [];
-  for (int i = 0; i < cities.length; i++) {
-    distances.add(travelDistance(cities[i], closest: closest));
-    await readFile();
+  for (int i = 0; i < paths.keys.length; i++) {
+    distances.add(travelDistance(paths.keys.elementAt(i), closest: closest));
+    await parseFile();
   }
   distances.sort();
   return closest ? distances.first : distances.last;
 }
 
-travelDistance(City city, {bool closest: true}) {
+travelDistance(String startingCity, {bool closest: true}) {
+  var current = startingCity;
+  String next;
   int distance = 0;
-  var current = city;
-  while (cities.isNotEmpty) {
-    City next = current.getNext(closest: closest);
-    if (next == null) break;
-
-    distance += current.distances[next];
-    cities.remove(current);
+  while ((next = getNext(current, closest: closest)) != null) {
+    distance += paths[current][next];
     visited.add(current);
     current = next;
   }
