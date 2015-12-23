da623de977e2aa8bab793f56ae3f507f8b4ecb65
Rudy Reeves <rudyreeves@lclark.edu>
2015-12-23 00:10:51 -0500

    Solved Day 9 (part 1)

--- bin/day9.dart
+++ bin/day9.dart
@@ -1,77 +1,133 @@
 import 'dart:io' show File;
 
+List<String> lines;
 List<Place> places = [];
-int shortest; // Initializes to null
+List<Place> visited = [];
 
 class Place {
-  final String from;
-  final String to;
-  final int distance;
-  const Place(this.from, this.to, this.distance); // Trivial constructor
-  toString() => '($from, $to, $distance)';
+  final String name;
+  Map<Place, int> routes;
+
+  static getPlaceByName(String name)
+    => places.where((place) => place.name == name);
+
+  Place getClosest() {
+    Place closest;
+    try {
+      closest = routes.keys.firstWhere((p) => !visited.contains(p));
+    } catch (_) {
+      return null;
+    }
+
+    int shortest = routes[closest];
+
+    for (Place route in routes.keys) {
+      if (visited.contains(route) || !places.contains(route)) continue;
+
+      if (routes[route] < shortest) {
+        shortest = routes[route];
+        closest = route;
+      }
+    }
+
+    return closest;
+  }
+
+  Place(this.name, this.routes);
+  toString() => '($name, ${routes.length})';
+}
+
+readFile() async {
+  places = [];
+  visited = [];
+
+  // Read in the file's if necessary
+  if (lines == null) {
+    lines = await new File('inputs/day9_input.txt').readAsLines();
+  }
+
+  for (String line in lines) {
+    List<String> tokens = line.split(' ');
+    String name = tokens[0];
+    String route = tokens[2];
+    int distance = int.parse(tokens[4]);
+
+    // Lookup the place
+    Place place;
+    var lookup = Place.getPlaceByName(name);
+
+    if (lookup.isEmpty) {
+      // Encountered a new city
+      place = new Place(name, {});
+      places.add(place);
+    } else {
+      // Get a handle on the existing city
+      place = lookup.single;
+    }
+
+    // Lookup the route
+    Place routePlace;
+    var routeLookup = Place.getPlaceByName(route);
+
+    if (routeLookup.isEmpty) {
+      // Encountered a new city
+      routePlace = new Place(route, {place: distance});
+      places.add(routePlace);
+    } else {
+      // Get a handle on the existing city
+      routePlace = routeLookup.single;
+    }
+
+    // Make place route to routePlace and vice-versa
+    place.routes[routePlace] = distance;
+    routePlace.routes[place] = distance;
+  }
 }
 
 main() async {
-  List<String> lines = await new File('inputs/day9_input.txt').readAsLines();
+  await readFile();
+  final n = places.length;
 
-//  Mock data
-//  List lines = [
-//    'London to Dublin = 464',
-//    'London to Belfast = 518',
-//    'Dublin to Belfast = 141'
-//  ];
+  int shortest;
+  int index;
 
-  for (String line in lines) {
-    // Parse input
-    List<String> tokens = line.split(' ');
-    String from = tokens[0];
-    String to = tokens[2];
-    int distance = int.parse(tokens[4]);
-
-    places.add(new Place(from, to, distance)); // Add 2-way city connections
-    places.add(new Place(to, from, distance)); // Add 2-way city connections
-  }
-
-  // Gets *all* the permutations of the places
-  List permutations = permute(places);
-
-  // Assume the first permutation is the shortest
-  int shortest = routeDistance(permutations.first);
-
-  // Loop through the permutations and try to find a shorter one
-  for (List permutation in permutations) {
-    int dist = routeDistance(permutation);
-    if (dist < shortest) {
-      shortest = dist;
+  for (int i = 0; i < n; i++) {
+    int distance = takeRoute(places[i]);
+    if (shortest == null || distance < shortest) {
+      shortest = distance;
+      index = i;
     }
+    await readFile();
   }
 
-  print(shortest);
+  print("\nWinner: $shortest (${places[index].name})");
 }
 
-// Gets the *total* distance of the route in the places[] argument
-int routeDistance(List<Place> places) {
-  int dist = 0;
-  for (int i = 1; i < places.length; i++) {
-    dist += places[i].distance;
-  }
-  return dist;
-}
+takeRoute(Place place) {
+  var sb = new StringBuffer();
+  sb.write(place.name);
 
-// Takes any list and returns a list of *all* its permutations
-// It's reflexive; the logic here is convoluted, but it works
-List<List> permute(List list) {
-  if (list.isEmpty) return [[]];
+  int distance = 0;
+  Place current = place;
 
-  Place firstElement = list.removeAt(0);
-  List returnValue = [];
-  List permutations = permute(list);
-  for (List permutation in permutations) {
-    for (int i = 0; i <= permutation.length; i++) {
-      List temp = new List.from(permutation);
-      temp.insert(i, firstElement);
-      returnValue.add(temp);
-    }
+
+  while (places.isNotEmpty) {
+    Place closest = current.getClosest();
+    if (closest == null) break;
+    visited.add(closest);
+
+    sb.write(" ->  ${closest.name}");
+    distance += current.routes[closest];
+
+    places.remove(current);
+    closest.routes.remove(place);
+    current.routes.remove(place);
+    closest.routes.remove(current);
+    current.routes.remove(closest);
+
+    current = closest;
   }
-  return returnValue;
+
+  print('$distance: $sb');
+  return distance;
 }
