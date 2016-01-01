f6eabe7455128796119521e487c290225e30f905
Rudy Reeves <rudyreeves@lclark.edu>
2016-01-01 12:49:26 -0500

    Completed day 14 (parts 1 and 2)

--- bin/day14.dart
+++ bin/day14.dart
@@ -6,27 +6,37 @@ const N = 2503;
 
 main() async {
   lines = await new File('inputs/day14_input.txt').readAsLines();
-  List reindeer = [];
+  List stats = [];
+  int maxTravel;
   for (String line in lines) {
     var tokens = line.split(' ');
+    var name = tokens[0];
     var speed = int.parse(tokens[3]);
     var endurance = int.parse(tokens[6]);
     var rest = int.parse(tokens[13]);
-    reindeer.add([speed, endurance, rest, 0]);
+    int travel = distanceTraveled(N, speed, endurance, rest);
+    if (maxTravel == null || travel > maxTravel) {
+      maxTravel = travel;
+    }
+    stats.add([name, speed, endurance, rest, 0]);
   }
-  for (int i = 0; i < N; i++) {
-    var winner = null;
-    int max = 0;
-    for (var r in reindeer) {
-      int dist = distanceTraveled(i, r[0], r[1], r[2]);
-      if (dist > max) {
+
+  for (int i = 1; i <= N; i++) {
+    var max, currentWinners;
+    for (var stat in stats) {
+      int dist = distanceTraveled(i, stat[1], stat[2], stat[3]);
+      if (max == null || dist > max) {
         max = dist;
-        winner = r;
+        currentWinners = [stat];
+      } else if (dist == max) {
+        currentWinners.add(stat);
       }
     }
-    winner[3]++;
+    currentWinners.forEach((w) => w[4]++);
   }
-  print(reindeer);
+  var finalWinner = stats..sort((s1, s2) => s2[4] - s1[4]);
+  print('Part 1: $maxTravel');
+  print('Part 2: ${finalWinner.first[4]}');
 }
 
 distanceTraveled(int n, int speed, int endurance, int rest) {
